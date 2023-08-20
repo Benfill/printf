56c4888d55f02b1f8a0b0fe49ed5619b93d1a697
achrafedd <achrafeddiba@gmail.com>
2023-08-20 19:33:04 +0100

    upload main.h

--- main.h
+++ main.h
@@ -11,6 +11,7 @@ int _putchar(char c);
 int _printf(const char *format, ...);
 void case_searcher(va_list, const char *, int *, int *);
 void case_searcher_2(va_list, const char *, int *, int *);
+void _case_searcher_3(va_list, const char *, int *, int *);
 int _putstr(char *str, char case_s);
 int _binary(int n);
 void _putnbr(int n, int *len);
@@ -20,5 +21,6 @@ void _hexadecimal(unsigned int n, int *len, char case_x);
 int _putptr(void *ptr);
 int _reverse(char *str);
 void rot13(char *str, int*);
-
+void _long_short(va_list args, char flag, char c, int *len);
+void _long_short_2(va_list args, char flag, char c, int *len);
 #endif
