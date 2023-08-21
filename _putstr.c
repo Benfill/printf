92b3d12b4208f8868c1059d4c6ca7824db8bb679
Benfill <anasse.benfillous@gmail.com>
2023-08-21 02:56:31 +0100

    updating reverse

--- _putstr.c
+++ _putstr.c
@@ -22,7 +22,8 @@ int _putstr(char *str, char case_s)
 		{
 			if ((str[i] > 0 && str[i] < 32) || str[i] >= 127)
 			{
-				len += write(1, "\\x0", 3);
+				len += write(1, "\\x0", 2);
+				len += _putchar('0');
 				_hexadecimal(str[i], &len, 'X');
 				len++;
 			}
--- main.c
+++
@@ -1,9 +0,0 @@
-#include	"main.h"
-
-int main(void)
-{
-    _putstr("Hello, World!\n", 'S');
-    _putstr("This is a test.\n", 'S');
-    _putstr("Non-printable: \x01\x02\x03\x7F\x80\xFF\n", 'S');
-    return (0);
-}
