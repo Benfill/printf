a92dd7ac44809da29c9393ec2311e9f3359902f8
Benfill <anasse.benfillous@gmail.com>
2023-08-21 02:59:51 +0100

    updating reverse

--- _putstr.c
+++ _putstr.c
@@ -22,9 +22,8 @@ int _putstr(char *str, char case_s)
 		{
 			if ((str[i] > 0 && str[i] < 32) || str[i] >= 127)
 			{
-				len += write(1, "\\x0", 2);
+				len += write(1, "\\x0", 3);
 				_hexadecimal(str[i], &len, 'X');
-				len++;
 			}
 			else
 				len += _putchar(str[i]);
