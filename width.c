4b2bc0d37c29edd4ec2dc408fca1c67ae8dfe433
Benfill <anasse.benfillous@gmail.com>
2023-08-22 20:45:21 +0100

    fixing betty errors

--- width.c
+++ width.c
@@ -1,7 +1,12 @@
 #include	"main.h"
 
 /**
- *
+ * width - function counts the width
+ * @arg: pointer to args
+ * @format: the fixed string in printf
+ * @len: pointer to length counter
+ * @i: format's index
+ * @sign: checker if the spaces should be before or after arg
  */
 
 void	width(va_list arg, const char *format, int *len, int *i, char sign)
