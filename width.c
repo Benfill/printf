7f69e730b179a62b94cbb1ff6691021cbb8886bf
Benfill <anasse.benfillous@gmail.com>
2023-08-22 20:58:49 +0100

    fixing width

--- width.c
+++ width.c
@@ -11,7 +11,7 @@
 
 void	width(va_list arg, const char *format, int *len, int *i, char sign)
 {
-	int	n;
+	int	n, tmp;
 	int	index = 0;
 	char	p[1000];
 
@@ -24,8 +24,11 @@ void	width(va_list arg, const char *format, int *len, int *i, char sign)
 	n = atoi(p);
 	if (format[*i] != '\0')
 	{
+		tmp = (*len);
 		if (sign == '-')
 			case_searcher(arg, format, len, i);
+		tmp = (*len) - tmp;
+		n -= tmp;
 		while (n != 0)
 		{
 			(*len) += _putchar(' ');
